8908b41e26bcfeca998f4b0fbae7ddd9d59d89a7
Yavuz Karaca <yavkaraca@gmail.com>
2024-10-17 02:40:42 +0200

    All pages finished

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {SimulationComponent} from "./components/simulation/simulation.component";
+import {AboutComponent} from './components/about/about.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'simulation',
     component: SimulationComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 ];
