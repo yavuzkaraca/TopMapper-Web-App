2c5703b70afe4acad4235279c3f59355134ce8d0
Yavuz Karaca <yavkaraca@gmail.com>
2024-11-08 03:53:07 +0100

    Bug fix

--- front-end/src/app/components/simulation/configuration/configuration.component.ts
+++ front-end/src/app/components/simulation/configuration/configuration.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, ChangeDetectorRef} from '@angular/core';
-import { SimulationService } from "../../../services/simulation.service";
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { NgForOf, NgIf } from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {SimulationService} from "../../../services/simulation.service";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
 
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import { SubstrateComponent } from '../substrate/substrate.component';
+import {SubstrateComponent} from '../substrate/substrate.component';
 import {RouterLink} from '@angular/router';
-import {MatButton} from '@angular/material/button'; // adjust path if needed
-
+import {MatButton} from '@angular/material/button';
 
 
 /**
@@ -37,10 +36,10 @@ export class ConfigurationComponent implements OnInit {
 
   /** Available simulation types */
   configTypes = [
-    { id: 'CONTINUOUS_GRADIENTS', name: 'Continuous Gradients' },
-    { id: 'WEDGES', name: 'Wedges' },
-    { id: 'STRIPE', name: 'Stripe' },
-    { id: 'GAP', name: 'Gap' }
+    {id: 'CONTINUOUS_GRADIENTS', name: 'Continuous Gradients'},
+    {id: 'WEDGES', name: 'Wedges'},
+    {id: 'STRIPE', name: 'Stripe'},
+    {id: 'GAP', name: 'Gap'}
   ];
 
   /** Selected configuration type */
@@ -50,7 +49,7 @@ export class ConfigurationComponent implements OnInit {
   basicSettingsForm: FormGroup;
 
   stepDecisionForm: FormGroup = new FormGroup<any>({});
-  gcForm: FormGroup = new FormGroup<any>({});
+  // gcForm: FormGroup = new FormGroup<any>({});
   switchesForm: FormGroup = new FormGroup<any>({});
   adaptationForm: FormGroup = new FormGroup<any>({});
 
@@ -65,46 +64,43 @@ export class ConfigurationComponent implements OnInit {
    * @param simulationService Service to interact with backend simulation API.
    * @param fb FormBuilder to create reactive forms.
    * @param dialog
-   * @param cdr
    */
   constructor(
     private simulationService: SimulationService,
     private fb: FormBuilder,
     public dialog: MatDialog,
-    private cdr: ChangeDetectorRef // Inject ChangeDetectorRef here
   ) {
 
     this.basicSettingsForm = this.fb.group({
-        gc_count: [null, { updateOn: 'blur' }],
-        gc_size: [null, { updateOn: 'blur' }],
-        step_size: [null, { updateOn: 'blur' }],
-        step_num: [null, { updateOn: 'blur' }]
+      gc_count: [null],
+      gc_size: [null],
+      step_size: [null],
+      step_num: [null]
     });
 
     this.stepDecisionForm = this.fb.group({
-        x_step_possibility: [null, { updateOn: 'blur' }],
-        y_step_possibility: [null, { updateOn: 'blur' }],
-        sigmoid_steepness: [null, { updateOn: 'blur' }],
-        sigmoid_shift: [null, { updateOn: 'blur' }],
-        sigmoid_height: [null, { updateOn: 'blur' }],
-        sigma: [null, { updateOn: 'blur' }],
-        force: [null, { updateOn: 'blur' }]
+      x_step_possibility: [null],
+      y_step_possibility: [null],
+      sigma: [null],
+      force: [null]
     });
 
     this.switchesForm = this.fb.group({
-        forward_sig: [null, { updateOn: 'blur' }],
-        reverse_sig: [null, { updateOn: 'blur' }],
-        ff_inter: [null, { updateOn: 'blur' }],
-        ft_inter: [null, { updateOn: 'blur' }]
+      forward_sig: [null],
+      reverse_sig: [null],
+      ff_inter: [null],
+      ft_inter: [null],
+      sigmoid_steepness: [null],
+      sigmoid_shift: [null],
+      sigmoid_height: [null]
     });
 
     this.adaptationForm = this.fb.group({
-        adaptation_enabled: [null, { updateOn: 'blur' }],
-        adaptation_mu: [null, { updateOn: 'blur' }],
-        adaptation_lambda: [null, { updateOn: 'blur' }],
-        adaptation_history: [null, { updateOn: 'blur' }]
+      adaptation_enabled: [null],
+      adaptation_mu: [null],
+      adaptation_lambda: [null],
+      adaptation_history: [null]
     });
-
 
 
   }
@@ -141,7 +137,7 @@ export class ConfigurationComponent implements OnInit {
     if (this.defaultConfig && this.selectedConfigType) {
       this.currentConfig = this.defaultConfig[this.selectedConfigType];
     }
-    // this.initForm();
+    this.initForm();
   }
 
   /** Toggles advanced settings visibility */
@@ -150,24 +146,21 @@ export class ConfigurationComponent implements OnInit {
   }
 
   openSubstrateDialog(): void {
-  const dialogRef = this.dialog.open(SubstrateComponent, {
-    width: '400px', // Set width as desired
-    data: {} // Pass any data if necessary
-  });
+    const dialogRef = this.dialog.open(SubstrateComponent, {
+      width: '400px', // Set width as desired
+      data: {} // Pass any data if necessary
+    });
 
-  // Optional: Handle dialog close event
-  dialogRef.afterClosed().subscribe(result => {
-    console.log('Dialog closed', result);
-    // Perform actions after dialog closes, if needed
-  });
-}
-
+    // Optional: Handle dialog close event
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('Dialog closed', result);
+      // Perform actions after dialog closes, if needed
+    });
+  }
 
 
   /** Combines all form values into a single configuration object */
   private getMergedFormValues() {
-
-    console.log(this.basicSettingsForm.value)
     return {
       ...this.basicSettingsForm.value,
       ...this.stepDecisionForm.value,
@@ -188,42 +181,40 @@ export class ConfigurationComponent implements OnInit {
   /** Initializes form groups with current configuration values */
 // Use detectChanges in initForm() after patching values
   private initForm() {
-      this.basicSettingsForm.patchValue({
-          gc_count: this.currentConfig.gc_count,
-          gc_size: this.currentConfig.gc_size,
-          step_size: this.currentConfig.step_size,
-          step_num: this.currentConfig.step_num
-      });
-      this.basicSettingsForm.updateValueAndValidity();
+    this.basicSettingsForm.patchValue({
+      gc_count: this.currentConfig.gc_count,
+      gc_size: this.currentConfig.gc_size,
+      step_size: this.currentConfig.step_size,
+      step_num: this.currentConfig.step_num
+    });
+    this.basicSettingsForm.updateValueAndValidity();
 
-      this.stepDecisionForm.patchValue({
-          x_step_possibility: this.currentConfig?.x_step_possibility,
-          y_step_possibility: this.currentConfig?.y_step_possibility,
-          sigmoid_steepness: this.currentConfig?.sigmoid_steepness,
-          sigmoid_shift: this.currentConfig?.sigmoid_shift,
-          sigmoid_height: this.currentConfig?.sigmoid_height,
-          sigma: this.currentConfig?.sigma,
-          force: this.currentConfig?.force
-      });
-      this.stepDecisionForm.updateValueAndValidity();
+    this.stepDecisionForm.patchValue({
+      x_step_possibility: this.currentConfig?.x_step_possibility,
+      y_step_possibility: this.currentConfig?.y_step_possibility,
+      sigma: this.currentConfig?.sigma,
+      force: this.currentConfig?.force
+    });
+    this.stepDecisionForm.updateValueAndValidity();
 
-      this.switchesForm.patchValue({
-          forward_sig: this.currentConfig?.forward_sig,
-          reverse_sig: this.currentConfig?.reverse_sig,
-          ff_inter: this.currentConfig?.ff_inter,
-          ft_inter: this.currentConfig?.ft_inter,
-      });
-      this.switchesForm.updateValueAndValidity();
+    this.switchesForm.patchValue({
+      forward_sig: this.currentConfig?.forward_sig,
+      reverse_sig: this.currentConfig?.reverse_sig,
+      ff_inter: this.currentConfig?.ff_inter,
+      ft_inter: this.currentConfig?.ft_inter,
+      sigmoid_steepness: this.currentConfig?.sigmoid_steepness,
+      sigmoid_shift: this.currentConfig?.sigmoid_shift,
+      sigmoid_height: this.currentConfig?.sigmoid_height,
+    });
+    this.switchesForm.updateValueAndValidity();
 
-      this.adaptationForm.patchValue({
-          adaptation_enabled: this.currentConfig?.adaptation_enabled,
-          adaptation_mu: this.currentConfig?.adaptation_mu,
-          adaptation_lambda: this.currentConfig?.adaptation_lambda,
-          adaptation_history: this.currentConfig?.adaptation_history,
-      });
-      this.adaptationForm.updateValueAndValidity();
+    this.adaptationForm.patchValue({
+      adaptation_enabled: this.currentConfig?.adaptation_enabled,
+      adaptation_mu: this.currentConfig?.adaptation_mu,
+      adaptation_lambda: this.currentConfig?.adaptation_lambda,
+      adaptation_history: this.currentConfig?.adaptation_history,
+    });
+    this.adaptationForm.updateValueAndValidity();
 
-      // Trigger change detection to immediately update UI
-      this.cdr.detectChanges();
   }
 }
