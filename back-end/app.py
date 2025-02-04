628103c502439fb62b3ecdc9c1e7ffdaea197d78
Yavuz Karaca <yavkaraca@gmail.com>
2025-02-04 14:47:46 +0100

    deploy ready

--- back-end/app.py
+++ back-end/app.py
@@ -186,4 +186,6 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
--- front-end/src/environment.ts
+++ front-end/src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   // production: false,
-  apiUrl: 'http://127.0.0.1:5000/'  // Use your local network IP for development
+  apiUrl: 'http://172.22.67.140:5000/'  // Use your local network IP for development
 };
